4dfbe1b4d8c12f2f86bb1632f8fbfa04b4f82d77
ewa <ewa.solomiewicz@gmail.com>
2017-06-25 13:45:56 +0200

    Dodano style, widok i szablon dla pojedynczego wpisu na stronie.

--- mysite/settings.py
+++ mysite/settings.py
@@ -102,3 +102,10 @@ USE_TZ = True
 
 STATIC_URL = '/static/'
 STATIC_ROOT = os.path.join(BASE_DIR, "public", "static")
+STATICFILES_FINDERS = ( 
+    "django.contrib.staticfiles.finders.FileSystemFinder",
+    "django.contrib.staticfiles.finders.AppDirectoriesFinder", 
+)
+STATICFILES_DIRS = (
+    os.path.join(BASE_DIR, "static"),
+)
